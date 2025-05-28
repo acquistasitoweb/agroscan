bbf7576e152acf7ff3530d5eee873604eb713898
titaniumwolf <titaniumwolfteam@gmail.com>
2025-05-28 19:32:54 +0200

    disabilita lint su build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ...altre opzioni,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // ...qui puoi mettere altre config future!
 };
 
-export default nextConfig;
+module.exports = nextConfig;
